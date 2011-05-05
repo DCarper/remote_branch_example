f26836602c9aaf612aa22097200a47251dd2e0a6
dcarper <djcarper@me.com>
2011-05-05 18:12:20 -0400

    strategy

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,8 @@ role :db, domain, :primary => true # This is where Rails migrations will run
 
 set :runner, user
 
-set :deploy_to, "~dancarper/code/deployments/remote_branch"
+set :application, 'spring_cleaning'
+set :deploy_to, "~dancarper/code/deployments/#{application}"
 
 
 #$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
@@ -37,53 +38,29 @@ end
 
 namespace :deploy do
 
-	desc "setup"
-	task :setup do
-		run "mkdir -p #{shared_path}"
-		run "touch #{shared_path}/DEPLOY"
-		run "echo 0 > #{shared_path}/DEPLOY"
-		run "#{scm_command} clone #{repository} #{current_path}"
-	end
-    
-  desc "Deploy aw yeaaah!"
+	desc "Deploy the MFer"
   task :default do
     update
     restart
+    cleanup
   end
-  
+
+  desc "Setup a GitHub-style deployment."
+  task :setup, :except => { :no_release => true } do
+    run "git clone #{repository} #{current_path}"
+  end
+
   desc "Update the deployed code."
   task :update_code, :except => { :no_release => true } do
-
-    # read the latest deploy number from the shared DEPLOY file, increment, and write it back
-    #
-    # if it fails then the number will be 0
-    #
-    new_num = next_branch_num()
-    
-    deleting_deploy_num = new_num.to_i - 10
-
-    new_branch_name = "#{branch}#{new_num}"
-    branch_for_delete = "#{branch}#{deleting_deploy_num}"
-    
-    on_rollback { rollback }
-
-    # update the origin
-    run "cd #{current_path}; #{scm_command} fetch origin;"
-
-    # create a new tracking branch for the current version of that branch
-    run "cd #{current_path}; #{scm_command} branch -f --track #{new_branch_name} origin/#{branch};"
-
-    #switch to it
-    run "cd #{current_path}; #{scm_command} checkout #{new_branch_name};"
-
-    #delete the 10-ago-branch ... don't fail if it doesn't exist.
-    begin
-      run "cd #{current_path}; #{scm_command} branch -D #{branch_for_delete} >/dev/null 2>&1"
-    rescue
-    end
+    run "cd #{current_path}; git fetch origin; git reset --hard #{branch}"
   end
 
-  desc <<-DESC
+  desc "Rollback a single commit."
+  task :rollback, :except => { :no_release => true } do
+    set :branch, "HEAD^"
+    default
+  end
+	  desc <<-DESC
     Deploys and starts a `cold' application. This is useful if you have not \
     deployed your application before, or if your application is (for some \
     other reason) not currently running. It will deploy the code, run any \
@@ -103,28 +80,6 @@ namespace :deploy do
     no-op with gitty deployment
   DESC
   task :symlink, :except => { :no_release => true } do ; end
-
-  namespace "rollback" do
-    desc "Rollback a single commit."
-    task :code, :except => { :no_release => true } do
-
-			# read the latest deploy number from the shared DEPLOY file, decrement, and write it back
-			old_num = old_branch_num()
-
-			old_branch_name = "#{branch}#{old_num}"
-
-			# switch to the last branch
-			run "cd #{current_path}; #{scm_command} checkout #{old_branch_name}"
-
-			# restart
-			deploy.restart
-    end
-    
-    desc "Roll back some code"
-    task :default do
-      rollback.code
-    end
-  end
 
   desc "no op"
   task :cleanup do ; end
